03a0607fa4d4183d07e544d3fe2494b0747c0724
Michael Fuller <mgatelabs@users.noreply.github.com>
2018-01-28 23:35:17 -0600

    Update customize.php
    Adding in defaults

--- www/customize.php
+++ www/customize.php
@@ -1,5 +1,23 @@
 <?php
 	require_once('_required.php');
-	// Setup your garage config here
-	array_push($config, new GarageItem("dr", "Right Door", 0, 2, "http://localhost:8081", "http://localhost:8081"));
+
+	{
+		$item = new Item("lg", "Left Garage");
+		// No Image, default
+		$item->child(new NoPreview('lg-image'));
+		// You should edit this one to point to your left side pi
+		//$item->child(new ImagePreview('rg-image', 'http://address:8081', 0, false));
+		$item->child(new Clicker('lg-click', 'Open/Close', '/toggle.php?gpio=1'));
+		array_push($config, $item);
+	}
+
+	{
+		$item = new Item("rg", "Right Garage");
+		// No Image, default
+		$item->child(new NoPreview('rg-image'));
+		// You should edit this one to point to your right side pi, the main unit
+		//$item->child(new ImagePreview('rg-image', 'http://address:8081', 0, false));
+		$item->child(new Clicker('rg-click', 'Open/Close', '/toggle.php?gpio=0'));
+		array_push($config, $item);
+	}
 ?>
